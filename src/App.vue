10379ab845d6a8ee0d8f199114524d1597d284f6
ZhangZisu <admin@zhangzisu.cn>
2020-04-17 23:44:35 +0800

    Enhance mobile experiences

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
         <settings-menu/>
       </v-toolbar-items>
     </v-app-bar>
-    <v-navigation-drawer app expand-on-hover disable-resize-watcher disable-route-watcher permanent :value="true">
+    <v-navigation-drawer app expand-on-hover disable-resize-watcher disable-route-watcher v-model="drawer" :mobileBreakPoint="0">
       <v-list nav>
         <v-list-item href="https://zhangzisu.cn/" target="_blank">
           <v-list-item-avatar tile size="24">
@@ -44,13 +44,30 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Watch } from 'vue-property-decorator'
 import Background from '@/components/Background.vue'
 import AppFooter from '@/components/AppFooter.vue'
 import SettingsMenu from '@/components/SettingsMenu.vue'
 
 @Component({ components: { Background, AppFooter, SettingsMenu } })
-export default class App extends Vue {}
+export default class App extends Vue {
+  drawer = true
+
+  mounted () {
+    if (this.$vuetify.breakpoint.xs) {
+      setTimeout(() => {
+        this.drawer = false
+      }, 500)
+    }
+  }
+
+  @Watch('$vuetify.breakpoint.xs')
+  _wBreakpoint (cur: boolean, old: boolean) {
+    if (old !== this.drawer) {
+      this.drawer = !cur
+    }
+  }
+}
 </script>
 
 <style>
